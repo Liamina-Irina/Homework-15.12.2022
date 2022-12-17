9ce3405c934e91650ed3840749bffdbb3cf19abf
Irina <liamina2207@gmail.com>
2022-12-17 13:32:38 +0300

    Solved Task 8

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (number > 0)
+    while (i < number + 1)
+    {
+        Console.Write($"{i}, ");
+        i = i + 2;
+    }
+else if (number <= 0)
+{
+   Console.WriteLine("Вы ввели отрицательное число либо 0");
+}
